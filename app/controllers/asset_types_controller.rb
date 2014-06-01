33b20de2d825b0befe71fa2be24e52f489852337
codegard1 <sa.kierkegaard@gmail.com>
2014-06-01 20:39:21 +0000

    started writing controller logic for asset_types

--- app/controllers/asset_types_controller.rb
+++ app/controllers/asset_types_controller.rb
@@ -2,8 +2,17 @@ class AssetTypesController < ApplicationController
 
   def new
     @asset_type = AssetType.new
+    @asset_types = AssetType.all
   end
   
-  
+  def create
+    @asset_type = AssetType.create
+    if @asset_type.save 
+      render 'new'
+    end
+  end
 
+  def show
+    @asset_type = AssetType.where(:id => params[:id]).first
+  end  
 end
