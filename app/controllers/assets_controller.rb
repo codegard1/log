7c9ee477b91b8510b8cc8acea9e516a9668dbb0f
codegard1 <sa.kierkegaard@gmail.com>
2014-06-15 19:46:09 +0000

    implemented collection_select in form builder for Asset#new

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -4,11 +4,17 @@ class AssetsController < ApplicationController
   end
   
   def create
+    @asset = Asset.create(params[:asset].permit(:asset_type_id,:user_id))
+    if @asset.save 
+      redirect_to assets_path
+    end
   end
   
   def index
+    @assets = Asset.all
   end
   
   def show
+    @asset = Asset.where(:id => params[:id]).first
   end
 end
