9392e16b6e36e3326b50e4ff445adbaede793c56
codegard1 <sa.kierkegaard@gmail.com>
2014-06-23 20:53:06 +0000

    started work on Moves#new. See Project Notes in welcome#index for details.

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,13 +1,22 @@
 class MovesController < ApplicationController
   def new
+  	@move = Move.new
+    @user = User.where( :id => params[:id] ).first
+    @move_types = MoveType.all.sort_by! &:title
+    @assets = Asset.all.sort_by! &:asset_tag
   end
 
   def edit
   end
 
   def create
+  	@move = Move.create(params[:move].permit(:description,:user_id,:move_type_id,:asset_id))
+    if @move.save
+      redirect_to moves_path
+    end
   end
   
   def index
+  	@moves = Move.all.sort_by! &:created_at
   end
 end
