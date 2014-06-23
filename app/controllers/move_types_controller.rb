0422737386353f3d9d04bfb47ac8ad60393c0c00
codegard1 <sa.kierkegaard@gmail.com>
2014-06-23 19:22:40 +0000

    implemented the move_types controller for new, create, and index actions

--- app/controllers/move_types_controller.rb
+++ app/controllers/move_types_controller.rb
@@ -1,14 +1,21 @@
 class MoveTypesController < ApplicationController
   def new
+  	@move_type = MoveType.new
   end
 
   def edit
   end
   
   def create
+  	@move_type = MoveType.create(params[:move_type].permit(:title))
+  	if @move_type.save
+  		@status_message = 'Move type ' + @move_type.title + ' saved successfully!'
+  		redirect_to move_types_path
+  	end
   end
   
   def index
+  	@move_types = MoveType.all.sort_by! &:title
   end
   
 end
