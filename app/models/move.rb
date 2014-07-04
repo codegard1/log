ed47731604e35edb33fbabf0cad4cb22ee1d18e6
codegard1 <sa.kierkegaard@gmail.com>
2014-07-04 19:31:03 +0000

    added validation for id fields in move model 2

--- app/models/move.rb
+++ app/models/move.rb
@@ -3,7 +3,7 @@ class Move < ActiveRecord::Base
   belongs_to :move_type
   belongs_to :asset
   
-  validates :user_id, presence: true, numericality: true {only_integer: true}
-  validates :move_type_id, presence: true, numericality: true {only_integer: true}
-  validates :asset_id, presence: true, numericality: true {only_integer: true}
+  validates :user_id, presence: true, numericality:  {only_integer: true}
+  validates :move_type_id, presence: true, numericality:  {only_integer: true}
+  validates :asset_id, presence: true, numericality:  {only_integer: true}
 end
